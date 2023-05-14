6ccbecbd236f4331f3b09d30b2f16bca6ff2ec9c
CMasterM <97553498+CMasterM@users.noreply.github.com>
2023-05-13 23:29:33 -0700

    Clean up Request Sender
    adding method headers and fixing indentation for RequestSender.java

--- app/src/main/java/sayit/server/RequestSender.java
+++ app/src/main/java/sayit/server/RequestSender.java
@@ -8,9 +8,37 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * <p>
+ * A class that contains static methods to make requests to our HTTP server to access the busines logic of our project
+ * </p>
+ * <p>
+ * Send request for GET, which should return the question history. 
+ * Send Request for POST/OR Put which takes sends an audio file to the server for whisper and chat GPT. 
+ * Send DELETE request to either delete a single entry or clear all entriews
+ * </p>
+ * <p>
+ * The following assumptions are made for this class:
+ *     <ul>
+ *         <li> Calls from UI will contain endpoint and Audio File for POST or PUT exists.</li>
+ *     </ui>
+ * </p>
+ */
+
 public class RequestSender {
+
+    /*
+     * URL for the server
+     */
     private static final String BASE_URL = "http://localhost:8100";
 
+
+    /*
+     * Send get request
+     * @ param endpoint: command we have set up for the server.
+     * @ return will return the response from the server that can be picked up from the UI if successful and handled otherwise
+     * @ throws IOException
+     */
     public static String sendGetRequest(String endpoint) throws IOException {
 
         //set up method variables
@@ -38,33 +66,46 @@ public class RequestSender {
             } else {
                 return "Request Error";
             }
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
+        }   finally {
+                if (connection != null) {
+                    connection.disconnect();
+                
+                }
+            }   
+        //return server response
         return responseBuilder.toString().trim();
     }
 
-    public static String sendPostRequest(String endpoint, byte[] body) throws IOException {
+    /*
+     * Method to Send Post request
+     * @param endpoint, string for the command sent to the server
+     * @return returns a String containing the server response
+     */
+    public static String sendPostRequest(String endpoint) throws IOException {
         
+        //method varioables 
         String audioFilePath = "/path/to/audio/file.wav";
         String url = BASE_URL + endpoint;
         StringBuilder responseBuilder = new StringBuilder();
         URL requestUrl = new URL(url);
 
+        //make connection
         HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
         connection.setDoOutput(true);
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/octet-stream");
 
+        //convert audio file into bytes
         try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+
             FileInputStream fileInputStream = new FileInputStream(audioFilePath)) {
             byte[] buffer = new byte[8192];
             int bytesRead;
+
             while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-            outputStream.write(buffer, 0, bytesRead);
+                outputStream.write(buffer, 0, bytesRead);
             }
+
             outputStream.flush();
 
             // Get the response
@@ -74,19 +115,25 @@ public class RequestSender {
                     String line;
                     while ((line = reader.readLine()) != null) {
                     responseBuilder.append(line).append("\n");
+                    }
                 }
-                }
-            }else {
+            } else {
                 return "Request error";
             }
 
-    }  finally {
-       connection.disconnect();
+        } finally {
+            connection.disconnect();
        
-    }
+        }
+        //return server response
         return responseBuilder.toString().trim();
     }
 
+    /*
+     * Method to Send Delete Request
+     * @param endpoint, a string containing the command for the server, either for Delete or Clear
+     * @return a string with the server response
+     */
     public static String sendDeleteRequest(String endpoint) throws IOException {
 
         //set up method variables
@@ -119,6 +166,7 @@ public class RequestSender {
                 connection.disconnect();
             }
         }
+        //return server response
         return responseBuilder.toString().trim();
     }
 
--- app/src/test/java/sayit/server/sendRequestTest.java
+++ app/src/test/java/sayit/server/sendRequestTest.java
@@ -7,24 +7,24 @@ import static org.junit.jupiter.api.Assertions.*;
 //test the request sender with mock server and handler
 public class sendRequestTest{
 
-    RequestSender sender = new RequestSender();
+
     @Test
     public void testGet(){
-
+        RequestSender.sendGetRequest(null);
     }
 
     @Test
     public void testPost(){
-
+        RequestSender.sendGetRequest(null);
     }
 
     @Test
     public void testDelete(){
-
+        RequestSender.sendGetRequest(null);
     }
 
     @Test
     public void testPut(){
-
+        RequestSender.sendGetRequest(null);
     }
 }
