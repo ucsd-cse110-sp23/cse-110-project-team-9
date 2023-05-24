928008fab533a392aade0fdd012a215a14bb2150
James Shen <jamesshen2002@gmail.com>
2023-05-24 02:47:34 -0700

    fixed up the login UI to match requirements

--- app/src/main/java/sayit/frontend/EventHandlers.java
+++ app/src/main/java/sayit/frontend/EventHandlers.java
@@ -17,94 +17,77 @@ import static sayit.frontend.FrontEndConstants.*;
  */
 public final class EventHandlers {
 
-	// constants
-	private static final String emailHeader = "Email: ";
-	private static final String passwordHeader = "Passworrd ";
-	private static final String createAccountTitle = "Create Account";
-	private static final String loginTitle = "Login";
+    // constants
+    private static final String PASSWORD_HEADER = "Password: ";
+    private static final String VERFIY_PASSWORD_HEADER = "Verify Password";
+    private static final String INVALID_INPUT_PROMPT = "Invalid email/password";
+    private static final String VERIFICATION_FAILED_PROMPT = "Password Verification Failed";
 
-	/**
-	 * Handles the event when the user presses the button from the sidebar (the
-	 * question/answer history button).
-	 *
-	 * @param ui     The <c>MainUserInterface</c> object.
-	 * @param qa     The <c>QuestionAnswerEntry</c> object.
-	 * @param button The <c>QuestionButton</c> object.
-	 * @return An <c>ActionListener</c> object.
-	 */
-	public static ActionListener onQaButtonPress(MainUserInterface ui, QuestionAnswerEntry qa, QuestionButton button) {
-		return e -> {
-			ui.displayEntry(qa);
-			// track which button was last selected for deletion
-			ui.setSelectedButton(button);
-		};
-	}
+    /**
+     * Handles the event when the user presses the button from the sidebar (the
+     * question/answer history button).
+     *
+     * @param ui     The <c>MainUserInterface</c> object.
+     * @param qa     The <c>QuestionAnswerEntry</c> object.
+     * @param button The <c>QuestionButton</c> object.
+     * @return An <c>ActionListener</c> object.
+     */
+    public static ActionListener onQaButtonPress(MainUserInterface ui, QuestionAnswerEntry qa, QuestionButton button) {
+        return e -> {
+            ui.displayEntry(qa);
+            // track which button was last selected for deletion
+            ui.setSelectedButton(button);
+        };
+    }
 
-	/**
-	 * Handles the event when the user presses the create button.
-	 * 
-	 * @return An <c>ActionListener</c> object.
-	 */
-	public static ActionListener onCreateButtonPress() {
-		return e -> {
-			JPanel myPanel = new JPanel();
-			myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-			JPanel topPanel = new JPanel();
-			topPanel.add(new JLabel(emailHeader));
-			JTextField emailField = new JTextField(10);
-			topPanel.add(emailField);
-			myPanel.add(topPanel);
+    /**
+     * Handles the event when the user presses the create button.
+     * 
+     * @return An <c>ActionListener</c> object.
+     */
+    public static ActionListener onCreateButtonPress() {
+        return e -> {
+            // check valid input
+            if (LoginUserInterface.getInstance().getEmail().length() == 0
+                    || LoginUserInterface.getInstance().getPassword().length() == 0) {
+                JOptionPane.showMessageDialog(null, INVALID_INPUT_PROMPT);
+                return;
+            }
 
-			JPanel bottomPanel = new JPanel();
-			bottomPanel.add(new JLabel(passwordHeader));
-			JTextField passwordField = new JTextField(10);
-			bottomPanel.add(passwordField);
-			myPanel.add(bottomPanel);
+            JPanel myPanel = new JPanel();
+            myPanel.add(new JLabel(PASSWORD_HEADER));
+            JTextField passwordField = new JTextField(10);
+            myPanel.add(passwordField);
 
-			int result = JOptionPane.showConfirmDialog(null, myPanel, createAccountTitle, JOptionPane.OK_CANCEL_OPTION,
-					JOptionPane.PLAIN_MESSAGE);
-			if (result == JOptionPane.OK_OPTION) {
-				// TODO: ADD to database if user presses ok
-				System.out.println("Created Account!");
-				System.out.println("Email entered: " + emailField.getText());
-				System.out.println("Password enterd: " + passwordField.getText());
-				LoginUserInterface.getInstance().close(); //close the login UI
-				MainUserInterface.getInstance(); //start the main UI
-			} 
-		};
-	}
+            int result = JOptionPane.showConfirmDialog(null, myPanel, VERFIY_PASSWORD_HEADER,
+                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
-	/**
-	 * Handles the event when the user presses the login button.
-	 * 
-	 * @return An <c>ActionListener</c> object.
-	 */
-	public static ActionListener onLoginButtonPress() {
-		return e -> {
-			JPanel myPanel = new JPanel();
-			myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-			JPanel topPanel = new JPanel();
-			topPanel.add(new JLabel(emailHeader));
-			JTextField emailField = new JTextField(10);
-			topPanel.add(emailField);
-			myPanel.add(topPanel);
+            if (result == JOptionPane.OK_OPTION) {
+                if (passwordField.getText().equals(LoginUserInterface.getInstance().getPassword())) {
+                    // successful login
+                    LoginUserInterface.getInstance().close(); // close the login UI
+                    MainUserInterface.getInstance(); // start the main UI
+                } else {
+                    JOptionPane.showMessageDialog(null, VERIFICATION_FAILED_PROMPT);
+                }
+            }
+        };
+    }
 
-			JPanel bottomPanel = new JPanel();
-			bottomPanel.add(new JLabel(passwordHeader));
-			JTextField passwordField = new JTextField(10);
-			bottomPanel.add(passwordField);
-			myPanel.add(bottomPanel);
-
-			int result = JOptionPane.showConfirmDialog(null, myPanel, loginTitle, JOptionPane.OK_CANCEL_OPTION,
-					JOptionPane.PLAIN_MESSAGE);
-			if (result == JOptionPane.OK_OPTION) {
-				// TODO: Verify credentials to determine successful login or not
-				System.out.println("Logged in!");
-				System.out.println("Email entered: " + emailField.getText());
-				System.out.println("Password enterd: " + passwordField.getText());
-				LoginUserInterface.getInstance().close(); //close the login UI
-				MainUserInterface.getInstance(); //start the main UI
-			} 
-		};
-	}
+    /**
+     * Handles the event when the user presses the login button.
+     * 
+     * @return An <c>ActionListener</c> object.
+     */
+    public static ActionListener onLoginButtonPress() {
+        return e -> {
+            boolean verify = true; //verify email and password cobination
+            if(verify) {
+                LoginUserInterface.getInstance().close(); // close the login UI
+                MainUserInterface.getInstance(); // start the main UI
+            } else {
+                
+            }
+        };
+    }
 }
--- app/src/main/java/sayit/frontend/LoginUserInterface.java
+++ app/src/main/java/sayit/frontend/LoginUserInterface.java
@@ -7,25 +7,35 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
- * The login user interface for the application.  
+ * The login user interface for the application.
  */
 public class LoginUserInterface {
-	
-	//class variables
+
+    // class variables
     private final JFrame frame;
-    
-    //constants
-    private final Dimension buttonDimension = new Dimension(200, 100);
-	private static final String createAccountTitle = "Create Account";
-	private static final String loginTitle = "Login";
-	
-	private LoginUserInterface() {
-		frame = new JFrame();
+    private JTextField emailField;
+    private JTextField passwordField;
+
+    // constants
+    private final Dimension BUTTON_DIMENSION = new Dimension(150, 40);
+    private final Dimension TEXT_FIELD_DIMENSION = new Dimension(200, 40);
+    private static final String APP_NAME = "SAYIT_ASSISTANT 2.0";
+    private static final String CREATE_ACCOUNT_PROMPT = "Create Account";
+    private static final String LOGIN_PROMPT = "Login";
+    private static final String EMAIL_HEADER = "Email: ";
+    private static final String PASSWORD_HEADER = "Password: ";
+
+    private LoginUserInterface() {
+        frame = new JFrame(APP_NAME);
         this.frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         addComponentsToPane(this.frame.getContentPane());
         this.frame.pack();
@@ -37,10 +47,8 @@ public class LoginUserInterface {
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                int confirmClose = JOptionPane.showConfirmDialog(frame,
-                        CLOSE_WINDOW_TEXT, CLOSE_WINDOW_TITLE,
-                        JOptionPane.YES_NO_OPTION,
-                        JOptionPane.QUESTION_MESSAGE);
+                int confirmClose = JOptionPane.showConfirmDialog(frame, CLOSE_WINDOW_TEXT, CLOSE_WINDOW_TITLE,
+                        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 
                 // if user confirms closing the window, exit
                 if (confirmClose == JOptionPane.YES_OPTION) {
@@ -49,16 +57,15 @@ public class LoginUserInterface {
                 }
             }
         });
-	}
-	private static LoginUserInterface userInterface;
-	
+    }
+
+    private static LoginUserInterface userInterface;
+
     /**
      * <p>
      * Gets or creates a new instance of the <c>LoginUserInterface</c> class. This
-     * method is
-     * designed so that there can be at most one instance of the
-     * <c>LoginUserInterface</c> class
-     * at any point.
+     * method is designed so that there can be at most one instance of the
+     * <c>LoginUserInterface</c> class at any point.
      * </p>
      *
      * <p>
@@ -82,20 +89,56 @@ public class LoginUserInterface {
      * @param pane The pane to add the components to.
      */
     private void addComponentsToPane(Container contentPane) {
-		JButton createButton = new JButton(createAccountTitle);
-		createButton.setPreferredSize(buttonDimension);
+
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+        JPanel topPanel = new JPanel();
+        topPanel.add(new JLabel(EMAIL_HEADER));
+        emailField = new JTextField();
+        emailField.setPreferredSize(TEXT_FIELD_DIMENSION);
+        topPanel.add(emailField);
+        contentPane.add(topPanel);
+
+        JPanel middlePanel = new JPanel();
+        middlePanel.add(new JLabel(PASSWORD_HEADER));
+        passwordField = new JTextField();
+        passwordField.setPreferredSize(TEXT_FIELD_DIMENSION);
+        middlePanel.add(passwordField);
+        contentPane.add(middlePanel);
+
+        JPanel bottomPanel = new JPanel();
+        JButton createButton = new JButton(CREATE_ACCOUNT_PROMPT);
+        createButton.setPreferredSize(BUTTON_DIMENSION);
         createButton.addActionListener(EventHandlers.onCreateButtonPress());
-        contentPane.add(createButton, BorderLayout.WEST);
-        JButton loginButton = new JButton(loginTitle);
-		loginButton.setPreferredSize(buttonDimension);
+        bottomPanel.add(createButton);
+        JButton loginButton = new JButton(LOGIN_PROMPT);
+        loginButton.setPreferredSize(BUTTON_DIMENSION);
         loginButton.addActionListener(EventHandlers.onLoginButtonPress());
-        contentPane.add(loginButton, BorderLayout.EAST);
-	}
-    
+        bottomPanel.add(loginButton);
+        contentPane.add(bottomPanel);
+    }
+
     /**
      * Closes the LoginUserInterface
      */
     public void close() {
-    	frame.setVisible(false);
+        frame.setVisible(false);
+    }
+
+    /**
+     * Returns the text in the email field
+     * 
+     * @return the text in the email field
+     */
+    public String getEmail() {
+        return emailField.getText();
+    }
+
+    /**
+     * Returns the text in the password field
+     * 
+     * @return the text in the password field
+     */
+    public String getPassword() {
+        return passwordField.getText();
     }
 }
