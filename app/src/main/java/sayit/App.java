f6dde8e7a06b88b723e7544ad2343d4f489a2a6b
ewang2002 <37031713+ewang2002@users.noreply.github.com>
2023-05-24 17:41:37 -0700

    refactor: use builder pattern for Server

--- app/src/main/java/sayit/App.java
+++ app/src/main/java/sayit/App.java
@@ -3,6 +3,8 @@ package sayit;
 import sayit.frontend.MainUserInterface;
 import sayit.server.Server;
 import sayit.server.ServerConstants;
+import sayit.server.db.mongo.MongoAccountHelper;
+import sayit.server.db.mongo.MongoWrapper;
 import sayit.server.openai.ChatGpt;
 import sayit.server.openai.Whisper;
 import sayit.server.storage.TsvStore;
@@ -10,11 +12,16 @@ import sayit.server.storage.TsvStore;
 public class App {
     public static void main(String[] args) {
         new Thread(() -> {
-            Server s = new Server(TsvStore.createOrOpenStore("data.tsv"),
-                    ServerConstants.SERVER_HOSTNAME,
-                    ServerConstants.SERVER_PORT,
-                    new Whisper(ServerConstants.OPENAI_API_KEY),
-                    new ChatGpt(ServerConstants.OPENAI_API_KEY));
+            MongoWrapper mongo = MongoWrapper.getOrCreateInstance(ServerConstants.MONGO_URI);
+            Server s = Server.builder()
+                    .setHost(ServerConstants.SERVER_HOSTNAME)
+                    .setPort(ServerConstants.SERVER_PORT)
+                    .setWhisper(new Whisper(ServerConstants.OPENAI_API_KEY))
+                    .setChatGpt(new ChatGpt(ServerConstants.OPENAI_API_KEY))
+                    .setStorage(TsvStore.createOrOpenStore("data.tsv"))
+                    .setAccountHelper(MongoAccountHelper.getOrCreateInstance(mongo))
+                    .build();
+
             s.start();
         }).start();
         MainUserInterface.getInstance();
--- app/src/main/java/sayit/server/Server.java
+++ app/src/main/java/sayit/server/Server.java
@@ -3,6 +3,7 @@ package sayit.server;
 import com.sun.net.httpserver.HttpServer;
 import sayit.common.qa.QuestionAnswerEntry;
 import sayit.server.contexts.*;
+import sayit.server.db.common.IAccountHelper;
 import sayit.server.openai.IChatGpt;
 import sayit.server.openai.IWhisper;
 import sayit.server.storage.IStore;
@@ -23,13 +24,15 @@ public class Server {
     /**
      * Creates a new server.
      *
-     * @param storage The storage to use.
-     * @param host    The host to use.
-     * @param port    The port to use.
-     * @param whisper The <c>whisper</c> instance to use.
-     * @param chatgpt The <c>ChatGPT</c> instance to use.
+     * @param storage       The storage to use.
+     * @param accountHelper The account helper to use.
+     * @param host          The host to use.
+     * @param port          The port to use.
+     * @param whisper       The <c>whisper</c> instance to use.
+     * @param chatgpt       The <c>ChatGPT</c> instance to use.
      */
-    public Server(IStore<QuestionAnswerEntry> storage, String host, int port, IWhisper whisper, IChatGpt chatgpt) {
+    private Server(IStore<QuestionAnswerEntry> storage, IAccountHelper accountHelper,
+                   String host, int port, IWhisper whisper, IChatGpt chatgpt) {
         this._port = port;
 
         try {
@@ -43,12 +46,25 @@ public class Server {
 
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)
                 Executors.newFixedThreadPool(10);
-        this._server.createContext("/ask", new AskQuestionHandler(storage, whisper, chatgpt));
-        this._server.createContext("/history", new HistoryHandler(storage));
-        this._server.createContext("/delete-question", new DeleteHandler(storage));
-        this._server.createContext("/clear-all", new ClearAllHandler(storage));
+
+        if (storage != null) {
+            this._server.createContext("/ask", new AskQuestionHandler(storage, whisper, chatgpt));
+            this._server.createContext("/history", new HistoryHandler(storage));
+            this._server.createContext("/delete-question", new DeleteHandler(storage));
+            this._server.createContext("/clear-all", new ClearAllHandler(storage));
+        }
+
         this._server.createContext("/ping", new PingHandler());
         this._server.setExecutor(threadPoolExecutor);
+    }
+
+    /**
+     * Creates a new server builder.
+     *
+     * @return The server builder.
+     */
+    public static ServerBuilder builder() {
+        return new ServerBuilder();
     }
 
     /**
@@ -57,5 +73,107 @@ public class Server {
     public void start() {
         this._server.start();
         System.out.println("Server started on port " + this._port);
+    }
+
+    /**
+     * A builder for the <c>Server</c> class.
+     */
+    public static class ServerBuilder {
+        private IStore<QuestionAnswerEntry> _storage;
+        private IAccountHelper _accountHelper;
+        private String _host;
+        private int _port = -1;
+        private IWhisper _whisper;
+        private IChatGpt _chatgpt;
+
+        /**
+         * Sets the storage to use.
+         *
+         * @param storage The storage to use.
+         * @return The builder.
+         */
+        public ServerBuilder setStorage(IStore<QuestionAnswerEntry> storage) {
+            this._storage = storage;
+            return this;
+        }
+
+        /**
+         * Sets the account helper to use.
+         *
+         * @param accountHelper The account helper to use.
+         * @return The builder.
+         */
+        public ServerBuilder setAccountHelper(IAccountHelper accountHelper) {
+            this._accountHelper = accountHelper;
+            return this;
+        }
+
+        /**
+         * Sets the host to use.
+         *
+         * @param host The host to use.
+         * @return The builder.
+         */
+        public ServerBuilder setHost(String host) {
+            this._host = host;
+            return this;
+        }
+
+        /**
+         * Sets the port to use.
+         *
+         * @param port The port to use.
+         * @return The builder.
+         */
+        public ServerBuilder setPort(int port) {
+            this._port = port;
+            return this;
+        }
+
+        /**
+         * Sets the whisper instance to use.
+         *
+         * @param whisper The whisper instance to use.
+         * @return The builder.
+         */
+        public ServerBuilder setWhisper(IWhisper whisper) {
+            this._whisper = whisper;
+            return this;
+        }
+
+        /**
+         * Sets the chatgpt instance to use.
+         *
+         * @param chatgpt The chatgpt instance to use.
+         * @return The builder.
+         */
+        public ServerBuilder setChatGpt(IChatGpt chatgpt) {
+            this._chatgpt = chatgpt;
+            return this;
+        }
+
+        /**
+         * Builds the server.
+         *
+         * @return The server.
+         */
+        public Server build() {
+            if (this._port == -1) {
+                this._port = ServerConstants.SERVER_PORT;
+            }
+
+            if (this._host == null) {
+                this._host = ServerConstants.SERVER_HOSTNAME;
+            }
+
+            return new Server(
+                    this._storage,
+                    this._accountHelper,
+                    this._host,
+                    this._port,
+                    this._whisper,
+                    this._chatgpt
+            );
+        }
     }
 }
