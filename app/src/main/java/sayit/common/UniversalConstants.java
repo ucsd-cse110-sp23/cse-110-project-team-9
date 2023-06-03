9abcb6abdff3b0b007b0c3a4bc0e4a7ec7cf4144
ewang2002 <37031713+ewang2002@users.noreply.github.com>
2023-06-03 13:07:56 -0700

    feat: add setup email functionality to InputHandler

--- app/src/main/java/sayit/common/UniversalConstants.java
+++ app/src/main/java/sayit/common/UniversalConstants.java
@@ -6,6 +6,7 @@ public final class UniversalConstants {
     public static final String CLEAR_ALL = "CLEAR_ALL";
     public static final String DELETE_PROMPT = "DELETE_PROMPT";
     public static final String ERROR = "ERROR";
+    public static final String SETUP_EMAIL = "SETUP_EMAIL";
     public static final String ID = "id";
 
 
